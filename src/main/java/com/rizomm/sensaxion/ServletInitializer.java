7a9ff789d39c1d8874552d1d5e06e0ddbbd020b1
Anthony Callaert <callaertanthony@gmail.com>
2016-01-22 20:23:36 +0100

    first commit

---
+++ src/main/java/com/rizomm/sensaxion/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.rizomm.sensaxion;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SensaxionApplication.class);
+	}
+
+}
